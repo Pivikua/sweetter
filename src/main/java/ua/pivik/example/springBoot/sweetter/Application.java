89d774be037a046938dee072d1b55d361b195950
Pivikua <pivikua@gmail.com>
2020-12-30 11:42:39 +0200

    Initial commit

---
+++ src/main/java/ua/pivik/example/springBoot/sweetter/Application.java
@@ -0,0 +1,15 @@
+package ua.pivik.example.springBoot.sweetter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @autor Alexander Pivovarov
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
